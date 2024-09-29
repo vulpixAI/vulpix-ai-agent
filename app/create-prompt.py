686184879639124a2207589252bf0f09943b642c
renancasalle <renan.goncalves@sptech.school>
2024-09-29 19:33:08 -0300

    Add empty __init__.py files to mysql and mysqlx packages

---
+++ app/create-prompt.py
@@ -0,0 +1,55 @@
+import google.generativeai as genai
+import psycopg2
+import json 
+
+GOOGLE_API_KEY = 'AIzaSyD6kN7Rp_UHJS6yRzqOOolf0E4NTyjM4iI'
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+connection = psycopg2.connect( 
+    database="vulpix",
+    host="localhost",
+    user="postgres",
+    password="30112004",
+    port="5432"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT informacao FROM informacoes WHERE id = 2;")
+info_user = cursor.fetchone()
+
+cursor.close()
+connection.close()
+
+#if info_user is None:
+#   print("Nenhum dado encontrado.")
+#else:
+#    print("Data from DB:", info_user[0])
+
+def create_prompt(info_user):
+    form_dict = info_user[0]  # O info_user[0] é um dicionário
+
+    form_str = json.dumps(form_dict, indent=4)
+
+    prompt = form_str + """
+    I will provide you with detailed information about a company, including its name, sector, branding, and target audience. Your task is to generate a high-quality English prompt for Stable Diffusion, which will 
+    create a visual representation based on the company's values, products, and aesthetic preferences. The generated prompt should include the following key elements:
+    Overall Concept: A brief description of the visual goal for the image, incorporating the company's unique selling proposition (USP) and branding.
+    Background: A detailed description of the background design, colors, and visual style that align with the company's identity.
+    Foreground Elements: Focus on the main products or services of the company, emphasizing their features and appeal. 
+    Describe the positioning of these elements to create a balanced and visually engaging image.
+    Text Elements: Include any important marketing or promotional text that should be part of the image (for example, a call to action or slogan). 
+    Provide guidelines on font style and placement.
+    Color Palette: Define the primary, secondary, and accent colors used in the image, based on the company's brand guidelines.
+    Audience and Tone: Define the target audience and the overall tone or mood of the image.
+    Final Goal: Specify the intended emotional impact or action you want the viewer to take after seeing the image (for example, urgency to purchase or visit a website).
+    """
+
+    response = model.generate_content(prompt)
+
+    print(response)
+
+if info_user:
+    create_prompt(info_user)
