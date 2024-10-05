60ffb632cad34a598948e6d9b0782f3780203db1
renancasalle <renan.goncalves@sptech.school>
2024-10-05 01:36:49 -0300

    Refactor code to load Google API key from environment variables

--- app/create-prompt.py
+++ app/create-prompt.py
@@ -1,8 +1,12 @@
 import google.generativeai as genai
 import json 
 import psycopg2
+import os 
+from dotenv import load_dotenv
 
-GOOGLE_API_KEY = ''
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
