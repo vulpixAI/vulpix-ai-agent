cc9941691f427e3b5f13ee7ebb9f201958afcef0
renancasalle <renan.goncalves@sptech.school>
2024-10-06 18:41:34 -0300

    feat create file connect pgsql

---
+++ utils/db_connection.py
@@ -0,0 +1,12 @@
+import psycopg2
+import os
+
+def get_connection():
+    connection = psycopg2.connect(
+        host= os.getenv('DB_HOST'),
+        datasbe = os.getenv('DB_DATABASE'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+        port = os.getenv('DB_PORT')
+    )
+    return connection
