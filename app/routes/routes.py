f8b1cc7902ccb5d14502b9b2bfde4c63a4da72a0
renancasalle <renan.goncalves@sptech.school>
2025-04-19 17:14:04 -0300

    fix: update mock_json to use new image URLs

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -61,9 +61,9 @@ def generate_caption_route():
 def mock_json():
     image_urls = [
         "https://avatars.githubusercontent.com/u/83714306?v=4",
-        "https://media.licdn.com/dms/image/v2/D4D03AQHWCQmpIUBSzg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727672217059?e=2147483647&v=beta&t=0bGpO80n_89w-1KyOBNwZjPsnteTDDZGVyHoMguE_k0",
+        "https://sdmntpritalynorth.oaiusercontent.com/files/00000000-94a0-6246-b7dd-b3b51fc92194/raw?se=2025-04-19T20%3A47%3A19Z&sp=r&sv=2024-08-04&sr=b&scid=3c7a8a9e-9233-5528-9a15-cd10036e725e&skoid=a3336399-497e-45e5-8f28-4b88ecca3d1f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-19T18%3A28%3A05Z&ske=2025-04-20T18%3A28%3A05Z&sks=b&skv=2024-08-04&sig=7wK1bEUrz1U9t5ltCtYWwJw99MT5rcd24Q/ZqfHz31U%3D",
         "https://avatars.githubusercontent.com/u/142420909?v=4",
-        "https://media.licdn.com/dms/image/v2/D4D03AQGgU0JTKFdEcQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1703171602261?e=2147483647&v=beta&t=u4jvPyQXJLWxWkbEXcsEe_l7gzNGh0nbgp1Ohab81n0"
+        "https://sdmntpreastus2.oaiusercontent.com/files/00000000-51f4-51f6-8fbb-9af6abd712c2/raw?se=2025-04-19T21%3A12%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=5a095d6f-40e6-5f33-bf0f-372f87c12bc8&skoid=a3336399-497e-45e5-8f28-4b88ecca3d1f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-19T18%3A27%3A02Z&ske=2025-04-20T18%3A27%3A02Z&sks=b&skv=2024-08-04&sig=XKM/gZD9eJmP6p/ntn1rh9vqpGib062TfkwJyFuLLsI%3D"
     ]
     return {
         "image_urls": image_urls,
