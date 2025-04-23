54b404c4a9671bc9748a9ada25f8c33fed9eda58
renancasalle <renan.goncalves@sptech.school>
2025-04-22 21:56:50 -0300

    fix: update mock_json to use new image URLs

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -60,10 +60,10 @@ def generate_caption_route():
 
 def mock_json():
     image_urls = [
-        "https://avatars.githubusercontent.com/u/83714306?v=4",
-        "https://sdmntpritalynorth.oaiusercontent.com/files/00000000-94a0-6246-b7dd-b3b51fc92194/raw?se=2025-04-19T20%3A47%3A19Z&sp=r&sv=2024-08-04&sr=b&scid=3c7a8a9e-9233-5528-9a15-cd10036e725e&skoid=a3336399-497e-45e5-8f28-4b88ecca3d1f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-19T18%3A28%3A05Z&ske=2025-04-20T18%3A28%3A05Z&sks=b&skv=2024-08-04&sig=7wK1bEUrz1U9t5ltCtYWwJw99MT5rcd24Q/ZqfHz31U%3D",
-        "https://avatars.githubusercontent.com/u/142420909?v=4",
-        "https://sdmntpreastus2.oaiusercontent.com/files/00000000-51f4-51f6-8fbb-9af6abd712c2/raw?se=2025-04-19T21%3A12%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=5a095d6f-40e6-5f33-bf0f-372f87c12bc8&skoid=a3336399-497e-45e5-8f28-4b88ecca3d1f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-19T18%3A27%3A02Z&ske=2025-04-20T18%3A27%3A02Z&sks=b&skv=2024-08-04&sig=XKM/gZD9eJmP6p/ntn1rh9vqpGib062TfkwJyFuLLsI%3D"
+        "https://cdn.leonardo.ai/users/4f747aa7-238a-4900-bad3-93a7e2a41968/generations/5e617e68-a632-4607-9604-7d1825666f5f/Leonardo_Phoenix_A_vibrant_and_playful_image_capturing_the_joy_1.jpg?w=512",
+        "https://cdn.leonardo.ai/users/4f747aa7-238a-4900-bad3-93a7e2a41968/generations/d99af10c-c366-4963-82af-741bdd4ddf9f/Leonardo_Phoenix_Unlocking_Happy_FeetOverall_ConceptA_vibrant_3.jpg?w=512",
+        "https://cdn.leonardo.ai/users/4f747aa7-238a-4900-bad3-93a7e2a41968/generations/d99af10c-c366-4963-82af-741bdd4ddf9f/Leonardo_Phoenix_Unlocking_Happy_FeetOverall_ConceptA_vibrant_0.jpg?w=512",
+        "https://cdn.leonardo.ai/users/4f747aa7-238a-4900-bad3-93a7e2a41968/generations/5e617e68-a632-4607-9604-7d1825666f5f/Leonardo_Phoenix_A_vibrant_and_playful_image_capturing_the_joy_2.jpg?w=512"
     ]
     return {
         "image_urls": image_urls,
